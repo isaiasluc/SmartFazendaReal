00dbe6cd8a6ff5a41d12588aa7a93b597072accf
isaiasluc <isaiiaslucena@gmail.com>
2020-04-30 10:50:56 -0300

    Atualizado index e app.js

--- SFR WebApp/public/App.js
+++ SFR WebApp/public/App.js
@@ -13,100 +13,64 @@
     };
     firebase.initializeApp(config);
 
-    var db = firebase.database();
-  
-    // Cria os listeners dos dados no firebase
+    db = firebase.database();
+       
+    var refTanque1 = db.ref("Dados tanque 1 (Caixa d'agua)");
+    
 
-    const sistRef = db.ref("Status do Sistema");
-    sistRef.on('value', onNewData('currentSist', 'sistLineChart', "Status do Sistema", 'm'));
+   
+    refTanque1.on('child_added', function(childSnapshot, prevChildKey) {
+      let key = childSnapshot.val();
+      //var keys = Object.values(key);
+      //valornivelT1 = key['2- Nivel do tanque'];
+      valorvolT1 = key['3- Volume do tanque'];
+      dataArr = [];
+      dataArr.push(valorvolT1);
+      
+      console.log(dataArr);
+    });
 
     
-    const statusSist = document.getElementById("statusSist");
-    const statusT1 = document.getElementById("statusT1");
-    const statusT2 = document.getElementById("statusT2");
-    const volT1 = document.getElementById("volT1");
-    const volT2 = document.getElementById("volT2");
-    const tempBomba = document.getElementById("tempBomba");
+    
+    
 
-
-    // Registra as funções que atualizam os gráficos e dados atuais da telemetria
-    const volT1ref = firebase.database().ref().child("Volume do tanque 1");
-    volT1ref.on('value', onNewData('volT1', 'volT1LineChart' , "Volume do tanque 1", 'm3'));
-
-    const dbRef2 = firebase.database().ref().child("Nivel do tanque 1");
-    dbRef2.on('value', snap => statusT1.innerText = snap.val());
+    var refTanque2 = db.ref("Dados tanque 2 (Poço)");
+    refTanque2.on('child_added', function(childSnapshot, prevChildKey) {
+      let item = (childSnapshot.val());
+      valornivelT2 = item['2- Nivel do tanque'];
+      valorvolT2 = item['3- Volume do tanque'];
+    });
 
     
-    const volT2ref = firebase.database().ref().child("Volume do tanque 2");
+    
+
+    //Enviando para página HTML os dados de on/off, volume, nível dos tanques e temperatura da bomba
+    const volT1ref = db.ref("Volume do tanque 1");
     volT1ref.on('value', snap => volT1.innerText = snap.val() + "m3");
+
+    const volT2ref = db.ref("Volume do tanque 2");
     volT2ref.on('value', snap => volT2.innerText = snap.val() + "m3");
 
-    const tempBombaRef = firebase.database().ref().child("Temperatura da Bomba");
-    tempBombaRef.on('value', snap => tempBomba.innerText = snap.val() + "ºC");
+    const nivelT1ref = db.ref("Nivel do tanque 1");
+    nivelT1ref.on('value', snap => nivelT1.innerText = snap.val());
 
-    const dbRef = firebase.database().ref().child("Status do Sistema");
-    dbRef.on('value', snap => statusSist.innerText = snap.val());
+    const nivelT2ref = db.ref("Nível do tanque 2");
+    nivelT2ref.on('value', snap => nivelT2.innerText = snap.val());
 
-    const dbRef3 = firebase.database().ref().child("Nível do tanque 2");
-    dbRef3.on('value', snap => statusT2.innerText = snap.val());
- 
-  
-  
-  
-    // Registrar função de click no botão de Liga/Desliga
+    const tempBombaref = db.ref("temp");
+    tempBombaref.on('value', snap => tempBomba.innerText = snap.val() + "ºC");
+    
+    const sistRef = db.ref("Status do Sistema");
+    sistRef.on('value', snap => statusSist.innerText = snap.val());
+
+      // Registrar função de click no botão de Liga/Desliga
     var btnOn = document.getElementById('btn-On');
     btnOn.addEventListener('click', function(evt){
-      sistRef.set('1');
+      sistRef.set('Ligado');
     });
     var btnOff = document.getElementById('btn-Off');
     btnOff.addEventListener('click', function(evt){
-      sistRef.set('0');
+      sistRef.set('Desligado');
     });
   
 })();
-  
-  
-// Retorna uma função que de acordo com as mudanças dos dados
-// Atualiza o valor atual do elemento, com a metrica passada (currentValueEl e metric)
-// e monta o gráfico com os dados e descrição do tipo de dados (chartEl, label)
-function onNewData(currentValueEl, chartEl, label, metric){
-  return function(snapshot){
-    var readings = snapshot.val();
-    if(readings){
-        var currentValue;
-        var data = [];
-        for(var key in readings){
-          currentValue = readings[key]
-          document.getElementById(currentValueEl).innerText = currentValue + ' ' + metric;
-          data.push(currentValue);
-        }
-
-        
-        buildLineChart(chartEl, label, data);
-    }
-  }
-}
-
-// Constroi um gráfico de linha no elemento (el) com a descrição (label) e os
-// dados passados (data)
-function buildLineChart(el, label, data){
-  var elNode = document.getElementById(el);
-  new Chart(elNode, {
-    type: 'line',
-    data: {
-        labels: new Array(data.length).fill(""),
-        datasets: [{
-            label: label,
-            data: data,
-            borderWidth: 1,
-            fill: false,
-            spanGaps: false,
-            lineTension: 0.1,
-            backgroundColor: "#F9A825",
-            borderColor: "#F9A825"
-        }]
-    }
-  });
-}
-
-
