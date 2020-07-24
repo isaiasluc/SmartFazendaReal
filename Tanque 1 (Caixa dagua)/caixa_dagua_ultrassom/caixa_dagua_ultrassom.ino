ef88c1207f70517f8cdc28390de3dfcdc6882e9f
Isaías Lucena <isaiiaslucena@gmail.com>
2020-07-24 16:16:09 -0300

    updated ultrassom

--- Tanque 1 (Caixa dagua)/caixa_dagua_ultrassom/caixa_dagua_ultrassom.ino
+++ Tanque 1 (Caixa dagua)/caixa_dagua_ultrassom/caixa_dagua_ultrassom.ino
@@ -14,7 +14,7 @@ NTPClient timeClient(ntpUDP, "a.st1.ntp.br");
 #define TABLE_NAME "Dados tanque 1 (Caixa d'agua)"
 
 //Configuraçõs do WiFi
-#define WIFI_SSID "William_2.4GHZ" //Nome da Wifi
+#define WIFI_SSID "William" //Nome da Wifi
 #define WIFI_PASSWORD "camaleao" //Senha da Wifi
 
 //Definindo pinos para trigger e echo do sensor HCSR04 (Ultrassom)
@@ -29,13 +29,8 @@ JsonObject &root = jsonBuffer.createObject();
 
 // DEFININDO TICKER DE ATUALIZAÇÃO
 // -------------------------------
-Ticker ticker;
-bool publishNewState = true;
+Ticker tick;
 
-void publish(){
-  publishNewState = true;
-}
-#define PUBLISH_INTERVAL 1000*60*0.166
 
 // ------------------------------
 // FUNÇÃO DISTANCIA E FUNÇÃO QUE FILTRA A DISTÂNCIA
@@ -75,17 +70,22 @@ duration = pulseIn(echoPin, HIGH);
 // Calculating the distance
 distance= (duration*0.034)/2;
 
-return distance;
+return (distance*10);
 }
 // ------------------------------
-
-void setup() {
+void espInit() {
   //Iniciando comunicação serial
   Serial.begin(115200);
   delay(1000);
   pinMode(trigPin, OUTPUT); //Seta o trigPin como Output (saída)
   pinMode(echoPin, INPUT); //Seta o echoPin como Input (entrada)
-  //Iniciando conexão WiFi
+  
+  //timeClient.setTimeOffset(0); //Offset do NTP Client (-10800 para GTM -3:00hrs)
+  timeClient.begin(); //Inicia o NTP Client
+}
+
+void wifiInit() {
+//Iniciando conexão WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Conectando com ");
   Serial.println(WIFI_SSID);
@@ -101,67 +101,85 @@ void setup() {
   WiFi.setSleepMode(WIFI_NONE_SLEEP);
   WiFi.mode(WIFI_STA);
 
-  timeClient.setTimeOffset(0); //Offset do NTP Client (-10800 para GTM -3:00hrs)
-  timeClient.begin(); //Inicia o NTP Client
-
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); //Inicia uma instância do Firebase
   Firebase.setString("system_power", "Desligado"); //Seta o estado inicial do sistema para Desligado
-
-  //Registrando o Ticker para publicar de tempos em tempos
-  ticker.attach_ms(PUBLISH_INTERVAL, publish);
+  Serial.println("Iniciada instância do Firebase");
+  Serial.println("Setando estado inicial do sistema para Desligado");
 }
 
-void loop() {
+//----------------------------------------------------------------------------------------
+
+//FUNÇÃO PARA ATUALIZAR DATA E HORA
+
+int timeUpdate() {
   timeClient.update();
   unsigned long epochTime = timeClient.getEpochTime(); //Retorna o timestamp
-  String formattedTime = timeClient.getFormattedTime();
+  //String formattedTime = timeClient.getFormattedTime(); //Retorna hora formatada
+  
+  return epochTime;
+}
+//----------------------------------------------------------------------------------------
+
+void enviaDados() {
+  float caixaVol, caixaAlturaAgua;
+  int timestamp, caixaLevel;
+  String system_power;
 
   float x,x2;
-  x=(distancia()*10);
+  x=(distancia());
   x2=x*x;
 
   real = ((-0.0099145*x2)+(1.3416*x)-2.6411);
   filtrado = moving_average();
-  
-  float alturamedia_caixa, tank1_vol, alturaagua;
-  String tank1_level, system_power;
-  
+
   //Dimensões do reservatório em mm
-  float R=14, r=13, h=32;
+  float R=140, r=130, h=320;
 
   //LIGANDO O SISTEMA
   system_power=Firebase.getString("system_power");
 
-  if (system_power=="Ligado") {
-    alturamedia_caixa=filtrado;
-    alturaagua=h-alturamedia_caixa;
-    tank1_vol=(((3.1415*(alturaagua))*((R*R)+(R*r)+(r*r))/3)/100);
-    
-      if (alturamedia_caixa > 250) {
-        Serial.println("Nivel do tanque 1: LOW");
-        tank1_level = "LOW";
-      } else {
-        Serial.println("Nivel do tanque 1: FULL");
-        tank1_level = "FULL";
-      }
+   if (system_power == "Desligado") {
+    Serial.println("Sistema desligado");
+  } else if (system_power == "Ligado") {
+    caixaAlturaAgua=h-filtrado;
+    caixaVol=(((3.1415*(caixaAlturaAgua))*((R*R)+(R*r)+(r*r))/3)/1000);
 
-      root["alturamedia_caixa"] = alturamedia_caixa;
-      root["tank1_level"] = tank1_level;
-      root["tank1_vol"] = tank1_vol;
-      root["time"] = epochTime;
+    //Mandando os dados coletados para o Firebase
+      if (caixaAlturaAgua > 250) {
+        caixaLevel = 2; //High
+      } else if (caixaAlturaAgua > 100 && caixaAlturaAgua < 250) {
+        caixaLevel = 1; //Ok
+      } else if (caixaAlturaAgua < 100) {
+        caixaLevel = 0; //Low
+      }
+      
+      root["caixaAlturaAgua"] = caixaAlturaAgua;
+      root["caixaLevel"] = caixaLevel;
+      root["caixaVol"] = caixaVol;
+      root["timestamp"] = timestamp;
       root["system_power"] = system_power;
 
-      Firebase.setFloat("alturamedia_caixa", alturamedia_caixa);
-      Firebase.setString("tank1_level", tank1_level);
-      Firebase.push(TABLE_NAME, root);
+      Firebase.setInt("caixaLevel", caixaLevel);
+      Firebase.setFloat("caixaAlturaAgua", caixaAlturaAgua);
       
-    //Exibindo informações no Serial Monitor do Arduino IDE
-    Serial.print("Distancia em cm: ");
-    Serial.println(alturaagua);
-    delay(10000);
-    
-  } else if (system_power=="Desligado") {
-    Serial.println("Sistema desligado");
-    delay(2000);
-  }
+      Firebase.push(TABLE_NAME, root);
+
+      if (Firebase.failed()){
+        Serial.println("Setting Number Failed...");
+        Serial.println(Firebase.error());
+      }
+      
+      Serial.println("Dados enviados com sucesso!");
+      }
+ }
+
+
+void setup() {
+ espInit();
+ wifiInit();
+}
+
+void loop() {
+  enviaDados();
+  delay(10000);
 }
